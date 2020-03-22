6a0b983b3f0a6933e10fcbfe4f9349e36c2cb4d6
Adrien <adrien.masson@hotmail.fr>
2020-03-22 12:58:07 +0100

    catch data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { useQuery } from "@apollo/react-hooks";
+require("dotenv").config();
+
+const client = new ApolloClient({
+	uri: "https://api.github.com/graphql",
+	request: operation => {
+		operation.setContext({
+			headers: {
+				Authorization: `Bearer `,
+			},
+		});
+	},
+});
+
+const GET_USER_INFOS = gql`
+	query {
+		viewer {
+			avatarUrl
+		}
+	}
+`;
+
+client
+	.query({
+		query: GET_USER_INFOS,
+	})
+	.then(console.log);
+
 function App() {
+	const { loading, error, data } = useQuery(GET_USER_INFOS, {});
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+		<ApolloProvider client={client}>
+			<div>
+				<img src={client.data.viewer.avatarUrl} alt="Logo" />
+				<h2>My first Apollo app </h2>
+			</div>
+		</ApolloProvider>
 	);
+
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+	// 				Learn React
+	// 			</a>
+	// 		</header>
+	// 	</div>
+	// );
 }
 
 export default App;
