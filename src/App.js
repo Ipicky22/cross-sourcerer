68c9019695b004adc53d5dc712091cac67047b0a
Adrien <adrien.masson@hotmail.fr>
2020-03-22 15:07:38 +0100

    display data hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import ApolloClient, { gql } from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { Query } from "react-apollo";
-require("dotenv").config();
-
-const client = new ApolloClient({
-	uri: "https://api.github.com/graphql",
-	request: operation => {
-		operation.setContext({
-			headers: {
-				Authorization: `Bearer `,
-			},
-		});
-	},
-});
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 const GET_USER_INFOS = gql`
 	query {
@@ -25,32 +12,17 @@ const GET_USER_INFOS = gql`
 	}
 `;
 
-client
-	.query({
-		query: GET_USER_INFOS,
-	})
-	.then(console.log);
-
 function App() {
+	const { loading, error, data } = useQuery(GET_USER_INFOS);
+
+	if (loading) return null;
+	if (error) return `Error! ${error}`;
+
 	return (
-		<ApolloProvider client={client}>
-			<div>
-				<Query query={GET_USER_INFOS}>
-					{({ loading, error, data }) => {
-						if (loading) return <div>Fetching</div>;
-						if (error) return <div>Error</div>;
-
-						return (
-							<div>
-								<img src={data.viewer.avatarUrl} alt="Logo" />
-							</div>
-						);
-					}}
-				</Query>
-
-				<h2>My first Apollo app </h2>
-			</div>
-		</ApolloProvider>
+		<div>
+			<img src={data.viewer.avatarUrl} alt="Logo" />
+			<h2>My first Apollo app </h2>
+		</div>
 	);
 }
 
