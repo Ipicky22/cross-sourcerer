d91ed870ff103d206ed38af060c19b7eb84ae523
Adrien <adrien.masson@hotmail.fr>
2020-03-23 12:57:05 +0100

    init host

---
+++ precache-manifest.3b92cfac97736092104798a62369aeb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28ae6a5b6f21ae1d8d22ab924ff65d8",
+    "url": "/cross-sourcerer/index.html"
+  },
+  {
+    "revision": "bc01beda1d3a47ac6d73",
+    "url": "/cross-sourcerer/static/css/main.0d63faeb.chunk.css"
+  },
+  {
+    "revision": "f83af24f041078c0d7cf",
+    "url": "/cross-sourcerer/static/js/2.4d993c00.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/cross-sourcerer/static/js/2.4d993c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc01beda1d3a47ac6d73",
+    "url": "/cross-sourcerer/static/js/main.fea01e28.chunk.js"
+  },
+  {
+    "revision": "d16f2a17c4a2b802b558",
+    "url": "/cross-sourcerer/static/js/runtime-main.e0b4248e.js"
+  }
+]);
